b36581c3c1197338a30964022004c9652bb29d9d
jualshe <shevchenko.j@gmail.com>
2022-05-08 17:55:42 -0700

    Create percents.py

---
+++ percents.py
@@ -0,0 +1,11 @@
+def percents(x, y):
+    one_percent = x / 100
+    result = y / one_percent
+    return result
+
+
+def print_percents(x, y):
+    print(str(y) + ' is ' + str(percents(x, y)) + ' % of ' + str(x))
+
+
+print_percents(200, 50)
